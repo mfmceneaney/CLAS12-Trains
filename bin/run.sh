2bfd5450122243a69df2b8bced76488676485237
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-07-20 14:52:13 -0400

    Updated default file to run in `bin/run.sh`.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JM_OPTS="-Xms32m -Xmx1500m -XX:+UseG1GC"
-java -cp $CLASSPATH\:$C12TRAINS/lib/build/libs/lib.jar $JAVA_OPTS $JM_OPTS org.jlab.trains.LambdaTrain $@
+java -cp $CLASSPATH\:$C12TRAINS/lib/build/libs/lib.jar $JAVA_OPTS $JM_OPTS org.jlab.trains.MainLambdaFeedDown_TRUTHMATCHING $@
